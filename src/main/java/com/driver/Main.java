1c280714a535a0ee19da61f4169c2dd852cea624
Avusulapavanchary <pavancharynxs7@gmail.com>
2023-01-13 19:02:38 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,23 +1,23 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args){
-        Product p=new Product();
-        System.out.println(p.product(1, 4)+" "+ p.product(3,6,8)+" "+ p.product(1.5,5.6));
 
+    public static void main(String[] args) {
+
+        Product p = new Product();
+        System.out.println(p.product(4,5)+" "+p.product(3,5,6)+" "+p.product(2.3,4.6));
     }
     public static class Product{
-        public int product(int x, int y) {
-
+        public  static int product(int x, int y) {
             return x*y;
         }
-        public int product(int x, int y, int z) {
+        public  static int product(int x, int y, int z) {
             return x*y*z;
-
         }
-        public double product(double x, double y) {
-
-            return x*y;
+        public  static double product(double x, double y) {
+            return  x*y;
         }
     }
+
+
 }
