01bc742ead5b74fb096cd5a2465501840460fc3a
AvusulaPavan <pavancharyn7@gmail.com>
2023-01-14 14:00:08 +0530

    poly sol

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,23 +1,21 @@
 package com.driver;
 
 public class Main {
-
-    public static void main(String[] args) {
-
-        Product p = new Product();
-        System.out.println(p.product(4,5)+" "+p.product(3,5,6)+" "+p.product(2.3,4.6));
-    }
-    public static class Product{
-        public  static int product(int x, int y) {
+    public static class  Product{
+        public int product(int x, int y){
             return x*y;
         }
-        public  static int product(int x, int y, int z) {
+        public int product(int x, int y, int z){
             return x*y*z;
         }
-        public  static double product(double x, double y) {
-            return  x*y;
+        public double product(double x, double y){
+            return x*y;
         }
     }
-
-
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.5, 2.5);
+    }
 }
